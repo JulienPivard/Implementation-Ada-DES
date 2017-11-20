7459c69c32ca45e8ce9acaee531325c961eb9172
Ythildyr <pivardjulien@gmail.com>
2017-11-21 00:45:10 +0100

    Ajout d'un affichage de bloc

---
+++ application/src/des/bloc/des_p-bloc_32_p-bloc_io.ads
@@ -0,0 +1,13 @@
+package Des_P.Bloc_32_P.Bloc_IO is
+
+   --  Affiche chaque bits du bloc.
+   --  @param Bloc
+   --  Le bloc de 32 bits à afficher.
+   procedure Put (Bloc : Bloc_32_T);
+
+   --  Affiche sur une ligne chaque bits du bloc.
+   --  @param Bloc
+   --  Le bloc de 32 bits à afficher.
+   procedure Put_Line (Bloc : Bloc_32_T);
+
+end Des_P.Bloc_32_P.Bloc_IO;
