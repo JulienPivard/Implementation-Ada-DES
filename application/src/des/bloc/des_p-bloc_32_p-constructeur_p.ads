fd842c7ee4fd26daea0186e67ad022f7fd425218
Ythildyr <pivardjulien@gmail.com>
2017-11-20 23:53:42 +0100

    Ajout du constructeur de bloc de 32 bits

---
+++ application/src/des/bloc/des_p-bloc_32_p-constructeur_p.ads
@@ -0,0 +1,41 @@
+package Des_P.Bloc_32_P.Constructeur_P is
+
+   pragma Pure;
+
+   --  Constructeur de bloc de 32 bits
+   type Constructeur_Bloc_32_T is tagged private;
+
+   --  Le brut de 32 bits qui va être raffiné en
+   --  un bloc de 32 bits plus facile à manipuler.
+   type Bloc_32_Brut_T is mod 2**32;
+   for Bloc_32_Brut_T'Size use 32;
+
+   --  Initialise un nouveau bloc vide de 32 bits.
+   --  @param Constructeur
+   --  Le constructeur de bloc de 32 bits.
+   --  @param Brut
+   --  Le brut de 32 bits.
+   procedure Preparer_Nouveau_Bloc_32
+      (Constructeur : in out Constructeur_Bloc_32_T; Brut : Bloc_32_Brut_T);
+
+   --  Raffine le brut de 32 bits pour construire le bloc de 32 bits.
+   --  @param Constructeur
+   --  Le constructeur de bloc de 32 bits.
+   procedure Construire_Bloc (Constructeur : in out Constructeur_Bloc_32_T);
+
+   --  Récupère le bloc de 32 bits terminé.
+   --  @param Constructeur
+   --  Le constructeur de bloc de 32 bits.
+   --  @return Le bloc de 32 bits.
+   function Recuperer_Bloc_32 (Constructeur : Constructeur_Bloc_32_T) return
+      Bloc_32_T;
+
+private
+
+   type Constructeur_Bloc_32_T is tagged
+      record
+         Brut : Bloc_32_Brut_T;
+         Bloc : Bloc_32_T;
+      end record;
+
+end Des_P.Bloc_32_P.Constructeur_P;
