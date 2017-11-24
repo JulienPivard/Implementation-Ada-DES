e1ffc52a590d024865a4e1c4b92f38763f1186da
Asmodee <pivardjulien@gmail.com>
2017-11-24 12:57:38 +0100

    Ajustement de noms de runner de tests

--- tests/main_test_harnais.adb
+++ tests/main_test_harnais.adb
@@ -9,9 +9,9 @@ with Clef_Suite;
 
 procedure Main_Test_Harnais is
 
-   procedure runner_bloc is new AUnit.Run.Test_Runner
+   procedure runner_bloc_32 is new AUnit.Run.Test_Runner
       (Bloc_Suite.Fonc_Bloc_Suite);
-   procedure runner_constructeur is new AUnit.Run.Test_Runner
+   procedure runner_constructeur_bloc_32 is new AUnit.Run.Test_Runner
       (Constructeur_Suite.Fonc_Constructeur_Suite);
    procedure runner_sous_clef is new AUnit.Run.Test_Runner
       (Sous_Clef_Suite.Fonc_Sous_Clef_Suite);
@@ -29,8 +29,8 @@ procedure Main_Test_Harnais is
 
 begin
    AUnit.Reporter.Text.Set_Use_ANSI_Colors (reporter, True);
-   runner_bloc (reporter, options);
-   runner_constructeur (reporter, options);
+   runner_bloc_32 (reporter, options);
+   runner_constructeur_bloc_32 (reporter, options);
    runner_sous_clef (reporter, options);
    runner_clef (reporter, options);
 end Main_Test_Harnais;
