bf7ba2b2c4da25ceda3553be500c69322657cf09
Ythildyr <pivardjulien@gmail.com>
2017-12-20 13:26:58 +0100

    Ajustement des noms de variables

--- application/src/client.adb
+++ application/src/client.adb
@@ -42,16 +42,16 @@ procedure Client is
 
    package Lecteur_64_IO is new Ada.Sequential_IO
    (Des_P.Bloc_P.Bloc_64_P.Constructeur_P.Bloc_64_Brut_T);
-   c : Des_P.Bloc_P.Bloc_64_P.Constructeur_P.Constructeur_Bloc_64_T;
-   f : Lecteur_64_IO.File_Type;
-   brut : Des_P.Bloc_P.Bloc_64_P.Constructeur_P.Bloc_64_Brut_T;
-   --  brut : constant Des_P.Bloc_P.Bloc_64_P.Constructeur_P.Bloc_64_Brut_T :=
+
+   C_64 : Des_P.Bloc_P.Bloc_64_P.Constructeur_P.Constructeur_Bloc_64_T;
+   Brut : Des_P.Bloc_P.Bloc_64_P.Constructeur_P.Bloc_64_Brut_T;
+   --  Brut : constant Des_P.Bloc_P.Bloc_64_P.Constructeur_P.Bloc_64_Brut_T :=
    --  2#11110000_11110000_11110000_11110000_
    --  11110000_11110000_11110000_11110000#;
-   bloc : Des_P.Bloc_P.Bloc_64_P.Bloc_64_T;
+   Bloc : Des_P.Bloc_P.Bloc_64_P.Bloc_64_T;
 
-   bloc_64 : Des_P.Bloc_P.Bloc_64_P.Bloc_64_T;
-   pragma Unreferenced (bloc_64);
+   Fichier : Lecteur_64_IO.File_Type;
+
 
 begin
 
@@ -110,22 +110,22 @@ begin
          return;
       end if;
 
-      Lecteur_64_IO.Open (f, Lecteur_64_IO.In_File, Nom_Fichier);
+      Lecteur_64_IO.Open (Fichier, Lecteur_64_IO.In_File, Nom_Fichier);
    end Ouverture_Fichier;
 
    Lecture_Fichier :
    loop
-      exit Lecture_Fichier when Lecteur_64_IO.End_Of_File (f);
-      Lecteur_64_IO.Read (f, brut);
+      exit Lecture_Fichier when Lecteur_64_IO.End_Of_File (Fichier);
+      Lecteur_64_IO.Read (Fichier, Brut);
 
-      c.Preparer_Nouveau_Bloc_64 (brut);
-      c.Construire_Bloc;
-      bloc := c.Recuperer_Bloc_64;
+      C_64.Preparer_Nouveau_Bloc_64 (Brut);
+      C_64.Construire_Bloc;
+      Bloc := C_64.Recuperer_Bloc_64;
 
-      Des_P.Bloc_P.Bloc_64_P.Bloc_IO.Put_Line (bloc);
+      Des_P.Bloc_P.Bloc_64_P.Bloc_IO.Put_Line (Bloc);
    end loop Lecture_Fichier;
 
-   Lecteur_64_IO.Close (f);
+   Lecteur_64_IO.Close (Fichier);
 
    Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Success);
    return;
