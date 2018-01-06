21dbd7d53b9bf59df0a4b13d023d3420a9a580fc
Ythildyr <pivardjulien@gmail.com>
2018-01-06 19:34:04 +0100

    Mise en place xor entre bloc et clef

---
+++ application/src/des/filtres/des_p-bloc_xor_clef_p.adb
@@ -0,0 +1,39 @@
+with Des_P.Bloc_P;
+
+package body Des_P.Bloc_Xor_Clef_P is
+
+   ---------------------------------------------------------------------------
+   function "xor"
+      (
+         Gauche : Des_P.Bloc_P.Bloc_48_P.Bloc_48_T;
+         Droite : Des_P.Clef_P.Clef_48_Abs_P.Clef_48_Abs_T'Class
+      )
+      return Des_P.Bloc_P.Bloc_48_P.Bloc_48_T
+   is
+      use Des_P.Bloc_P.Bloc_48_P;
+      use Des_P.Clef_P.Clef_48_Abs_P;
+      use Des_P.Bloc_P;
+      B : Bloc_48_T;
+      Bit : Bit_T;
+   begin
+      for I in Intervalle_Bloc_48_T loop
+         Bit :=
+            Gauche.Lire_Bit (I) xor Droite.Lire_Bit (Intervalle_Clef_48_T (I));
+         B.Ecrire_Bit (I, Bit);
+      end loop;
+      return B;
+   end "xor";
+
+   ---------------------------------------------------------------------------
+   function "xor"
+      (
+         Gauche : Des_P.Clef_P.Clef_48_Abs_P.Clef_48_Abs_T'Class;
+         Droite : Des_P.Bloc_P.Bloc_48_P.Bloc_48_T
+      )
+      return Des_P.Bloc_P.Bloc_48_P.Bloc_48_T
+   is
+   begin
+      return Droite xor Gauche;
+   end "xor";
+
+end Des_P.Bloc_Xor_Clef_P;
---
+++ application/src/des/filtres/des_p-bloc_xor_clef_p.ads
@@ -0,0 +1,38 @@
+with Des_P.Bloc_P.Bloc_48_P;
+with Des_P.Clef_P.Clef_48_Abs_P;
+
+--  @summary
+--  L'opération xor entre un bloc de bits et une clef.
+--  @description
+--  Permet de faire un xor entre un bloc de 48 bits et
+--  une clef de même taille.
+--  @group XOR
+package Des_P.Bloc_Xor_Clef_P is
+
+   --  Le xor entre le bloc et la clef de 48 bits.
+   --  @param Gauche
+   --  Le bloc de 48 bits.
+   --  @param Droite
+   --  La clef de 48 bits.
+   --  @return Le résultat du xor.
+   function "xor"
+      (
+         Gauche : Des_P.Bloc_P.Bloc_48_P.Bloc_48_T;
+         Droite : Des_P.Clef_P.Clef_48_Abs_P.Clef_48_Abs_T'Class
+      )
+      return Des_P.Bloc_P.Bloc_48_P.Bloc_48_T;
+
+   --  Le xor entre le bloc et la clef de 48 bits.
+   --  @param Gauche
+   --  La clef de 48 bits.
+   --  @param Droite
+   --  Le bloc de 48 bits.
+   --  @return Le résultat du xor.
+   function "xor"
+      (
+         Gauche : Des_P.Clef_P.Clef_48_Abs_P.Clef_48_Abs_T'Class;
+         Droite : Des_P.Bloc_P.Bloc_48_P.Bloc_48_T
+      )
+      return Des_P.Bloc_P.Bloc_48_P.Bloc_48_T;
+
+end Des_P.Bloc_Xor_Clef_P;
