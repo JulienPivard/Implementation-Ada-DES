0db1e6b79b2120c2622764b9cc3fec4ed4bc4cd2
Ythildyr <pivardjulien@gmail.com>
2018-02-11 00:07:20 +0100

    Mise en place filtre étage d'entrée et tests unitaires

---
+++ tests/harnais/filtres/cryptage/ip_cryptage_suite_p.adb
@@ -0,0 +1,31 @@
+with AUnit.Test_Caller;
+
+with Des_P.Filtre_P.Cryptage_P.Ip_P.Test_P;
+use  Des_P.Filtre_P.Cryptage_P.Ip_P.Test_P;
+
+package body Ip_Cryptage_Suite_P is
+
+   package Caller is new AUnit.Test_Caller
+      (Test_Fixt_T);
+
+   ---------------------------------------------------------------------------
+   function Fonc_Suite
+      return AUnit.Test_Suites.Access_Test_Suite
+   is
+      ptr_test_suite : constant AUnit.Test_Suites.Access_Test_Suite :=
+         AUnit.Test_Suites.New_Suite;
+      nom_test : constant String := "(Tests filtre permutation initial) ";
+   begin
+
+      ptr_test_suite.all.Add_Test
+         (Caller.Create
+            (nom_test & "Permutation initial",
+            Test_Permutation_Initial'Access
+            )
+         );
+
+      return ptr_test_suite;
+
+   end Fonc_Suite;
+
+end Ip_Cryptage_Suite_P;
---
+++ tests/harnais/filtres/cryptage/ip_cryptage_suite_p.ads
@@ -0,0 +1,8 @@
+with AUnit.Test_Suites;
+
+package Ip_Cryptage_Suite_P is
+
+   function Fonc_Suite
+      return AUnit.Test_Suites.Access_Test_Suite;
+
+end Ip_Cryptage_Suite_P;
