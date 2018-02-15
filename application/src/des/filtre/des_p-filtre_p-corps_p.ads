67b97940721577f67bece2e9a10ed068ee9b2b34
Ythildyr <pivardjulien@gmail.com>
2018-02-15 11:34:39 +0100

    Ajustement numéro des filtres

--- application/src/des/filtre/des_p-filtre_p-corps_p.ads
+++ application/src/des/filtre/des_p-filtre_p-corps_p.ads
@@ -16,7 +16,7 @@ package Des_P.Filtre_P.Corps_P is
 
    --  Le numéro du filtre représente la position
    --  dans la suite des filtres
-   type Numero_Filtre_T is range 0 .. 17;
+   type Numero_Filtre_T is range 1 .. 16;
 
    --  Modifie le numéro du filtre.
    --  @param Filtre
@@ -42,7 +42,7 @@ private
    Table_Decalage : constant
       array (Numero_Filtre_T)
       of Des_P.Clef_P.Clef_56_Abs_P.Decalage_T :=
-      (0, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1, 0);
+      (1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1);
 
    type Corps_Abstrait_T is abstract new Filtre_Interface_T with null record;
 
