2a99d3cab6ceb2f8e62f67bba7e765dbc585591c
Ythildyr <pivardjulien@gmail.com>
2018-03-05 13:08:20 +0100

    Refactoring code execution cryptage decryptage

--- application/src/client.adb
+++ application/src/client.adb
@@ -169,69 +169,57 @@ procedure Client is
    ---------------------------------------------------------------------------
    procedure Executer_Crypt_Decrypt
       (
-         Faiseur : in out Faiseur_P.Constructeur_Interface_T'Class;
          Clef : Des_P.Clef_P.Clef_64_P.Clef_T;
          Nom_Fichier : String;
-         Action : Action_T
+         Action : Action_T;
+         C_Type : Chaine_Seq_Task_T
       );
 
    procedure Executer_Crypt_Decrypt
       (
-         Faiseur : in out Faiseur_P.Constructeur_Interface_T'Class;
          Clef : Des_P.Clef_P.Clef_64_P.Clef_T;
          Nom_Fichier : String;
-         Action : Action_T
+         Action : Action_T;
+         C_Type : Chaine_Seq_Task_T
       )
    is
-      F_56 : Faiseur_56_P.Constructeur_Clef_T;
-      F_48 : Faiseur_48_P.Constructeur_Clef_T;
+      Faiseur : Faiseur_P.Constructeur_Interface_T'Class :=
+         Init_Faiseur_Chaine (Action, C_Type);
+      Chaine : Des_P.Chaine_P.Chaine_Interface_T'Class :=
+         Init_Chaine (Faiseur, Clef);
+      Extension : constant String := Init_Extension (Action, C_Type);
+
+      Debut, Fin : Ada.Calendar.Time;
+      Duree : Duration;
+      package Duree_IO is new
+      Ada.Text_IO.Fixed_IO (Duration);
+      use type Ada.Calendar.Time;
    begin
-      Faiseur.Initialiser (F_56, F_48);
-      Faiseur.Construire (Clef);
-      Mesure_Temps :
-      declare
-         Chaine : Des_P.Chaine_P.Chaine_Interface_T'Class :=
-            Faiseur.Recuperer_Chaine;
+      Debut := Ada.Calendar.Clock;
+      Chaine.Filtrer
+         (
+            Nom_Fichier,
+            Extension
+         );
+      Fin := Ada.Calendar.Clock;
+      Duree := Fin - Debut;
 
-         Extension : constant String :=
-               (
-                  case Action is
-                     when Crypter => "crypt",
-                     when Decrypter => "decrypt"
-               );
-
-         Debut, Fin : Ada.Calendar.Time;
-         Duree : Duration;
-         package Duree_IO is new
-         Ada.Text_IO.Fixed_IO (Duration);
-         use type Ada.Calendar.Time;
-      begin
-         Debut := Ada.Calendar.Clock;
-         Chaine.Filtrer
-            (
-               Nom_Fichier,
-               Extension
-            );
-         Fin := Ada.Calendar.Clock;
-         Duree := Fin - Debut;
-
-         --------------------------------------
-         Ada.Text_IO.New_Line (1);
-         Ada.Text_IO.Put ("Temps séquentielle : ");
-         Ada.Text_IO.New_Line (1);
-         Duree_IO.Put (Duree);
-         Ada.Text_IO.Put_Line (" s");
-         if Duree > 60.0 then
-            Duree_IO.Put (Duree / 60.0);
-            Ada.Text_IO.Put_Line (" min");
-         end if;
-         if Duree > 3600.0 then
-            Duree_IO.Put (Duree / 3600.0);
-            Ada.Text_IO.Put_Line (" h");
-         end if;
-         Ada.Text_IO.New_Line (1);
-         --------------------------------------
-      end Mesure_Temps;
+      --------------------------------------
+      Ada.Text_IO.New_Line (1);
+      Ada.Text_IO.Put ("Temps séquentielle : ");
+      Ada.Text_IO.New_Line (1);
+      Duree_IO.Put (Duree);
+      Ada.Text_IO.Put_Line (" s");
+      if Duree > 60.0 then
+         Duree_IO.Put (Duree / 60.0);
+         Ada.Text_IO.Put_Line (" min");
+      end if;
+      if Duree > 3600.0 then
+         Duree_IO.Put (Duree / 3600.0);
+         Ada.Text_IO.Put_Line (" h");
+      end if;
+      Ada.Text_IO.New_Line (1);
+      --------------------------------------
    end Executer_Crypt_Decrypt;
 
    ---------------------------------------------------------------------------
@@ -350,9 +338,6 @@ begin
          (Position_Nom_Fic);
       Octets_En_Trop : Ada.Directories.File_Size;
       use type Ada.Directories.File_Size;
-
-      Faiseur : Faiseur_P.Constructeur_Interface_T'Class :=
-         Init_Faiseur_Chaine (Action, Sequentiel);
    begin
       if not Ada.Directories.Exists (Nom_Fichier) then
          Put_Line (Standard_Error, "██████ Erreur !");
@@ -387,7 +372,8 @@ begin
          return;
       end if;
 
-      Executer_Crypt_Decrypt (Faiseur, Clef, Nom_Fichier, Action);
+      Executer_Crypt_Decrypt (Clef, Nom_Fichier, Action, Sequentiel);
+      Executer_Crypt_Decrypt (Clef, Nom_Fichier, Action, Tache);
 
    end Ouverture_Fichier;
 
