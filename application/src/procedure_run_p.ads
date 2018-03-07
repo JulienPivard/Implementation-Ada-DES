cc08f19bf1adc835c9b62b86134389de3a5cb792
Ythildyr <pivardjulien@gmail.com>
2018-03-07 23:15:00 +0100

    Mise en privé des inclusion

--- application/src/procedure_run_p.ads
+++ application/src/procedure_run_p.ads
@@ -1,8 +1,8 @@
-with Des_P.Chaine_P.Constructeur_I_P;
 with Des_P.Clef_P.Clef_64_P;
 
-with Des_P.Clef_P.Clef_56_P.Constructeur_P;
-with Des_P.Clef_P.Clef_48_P.Constructeur_P;
+private with Des_P.Chaine_P.Constructeur_I_P;
+private with Des_P.Clef_P.Clef_56_P.Constructeur_P;
+private with Des_P.Clef_P.Clef_48_P.Constructeur_P;
 
 package Procedure_Run_P is
 
