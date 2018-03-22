70dc39e183be2e570ac4e89b98f8b7535fa107c9
Ythildyr <pivardjulien@gmail.com>
2018-03-22 21:44:33 +0100

    Suppression constructeur ravenscar dans tests unitaires chaine raven

--- tests/scenarios/chaine/ravenscar/des_p-chaine_p-ravenscar_p-test_p.adb
+++ tests/scenarios/chaine/ravenscar/des_p-chaine_p-ravenscar_p-test_p.adb
@@ -5,24 +5,19 @@ with Des_P.Clef_P.Clef_56_P.Constructeur_P;
 with Des_P.Clef_P.Clef_48_P.Constructeur_P;
 with Des_P.Clef_P.Clef_48_P;
 
-with Des_P.Chaine_P.Ravenscar_P.Constructeur_Cryptage_P;
-with Des_P.Chaine_P.Ravenscar_P.Constructeur_Decryptage_P;
-
 with Des_P.Chaine_P.Sequentiel_P;
 with Des_P.Chaine_P.Sequentiel_P.Constructeur_Cryptage_P;
 with Des_P.Chaine_P.Sequentiel_P.Constructeur_Decryptage_P;
+
+with Des_P.Filtre_P.Fabrique_P.Cryptage_P;
+with Des_P.Filtre_P.Fabrique_P.Decryptage_P;
+with Des_P.Faiseur_P;
 
 with Ada.Directories;
 
 with Ada.IO_Exceptions;
 
 package body Des_P.Chaine_P.Ravenscar_P.Test_P is
-
-   package Faiseur_Cryptage_P renames
-      Des_P.Chaine_P.Ravenscar_P.Constructeur_Cryptage_P;
-
-   package Faiseur_Decryptage_P renames
-      Des_P.Chaine_P.Ravenscar_P.Constructeur_Decryptage_P;
 
    package Faiseur_56_P renames Des_P.Clef_P.Clef_56_P.Constructeur_P;
 
@@ -56,7 +51,6 @@ package body Des_P.Chaine_P.Ravenscar_P.Test_P is
    ---------------------------------------------------------------------------
    ---------------------------------------------------------------------------
    procedure Test_Filtre_Crypt (T : in out Test_Fixt_T) is
-      Const_Crypt : Faiseur_Cryptage_P.Constructeur_Cryptage_T;
       Const_56 : Faiseur_56_P.Constructeur_Clef_T;
       Const_48 : Faiseur_48_P.Constructeur_Clef_T;
    begin
@@ -68,9 +62,31 @@ package body Des_P.Chaine_P.Ravenscar_P.Test_P is
          Lecteur_64_IO.Close (Fichier);
          pragma Unreferenced (Fichier);
       end;
-      Const_Crypt.Initialiser (Const_56, Const_48);
-      Const_Crypt.Construire (T.Clef);
-      T.Chaine := Chaine_T (Const_Crypt.Recuperer_Chaine);
+      declare
+         Clef_56 : Des_P.Clef_P.Clef_56_I_P.Clef_Interface_T'Class :=
+            Des_P.Faiseur_P.Faire_Clef (Const_56, T.Clef);
+         Fabrique : Des_P.Filtre_P.Fabrique_P.Cryptage_P.Fabrique_T;
+      begin
+         T.Chaine.Filtre_Entree := Des_P.Filtre_P.Entree_P.Holder_P.To_Holder
+               (Fabrique.Fabriquer_Entree);
+
+         --  Ajoute le filtre de corps à la chaine.
+         for I in Numero_Filtre_T'Range loop
+            --  Décalage à gauche pour le cryptage.
+            Clef_56.Decaler_Bits_A_Gauche (Table_Decalage (I));
+            --  Initialise le filtre avec la clef de 48.
+            T.Chaine.Filtres_Corps (I) :=
+               Des_P.Filtre_P.Corps_P.Holder_P.To_Holder
+               (
+                  Fabrique.Fabriquer_Corps
+                  (Des_P.Faiseur_P.Faire_Clef (Const_48, Clef_56))
+               );
+         end loop;
+
+         --  Ajoute le filtre de sortie.
+         T.Chaine.Filtre_Sortie := Des_P.Filtre_P.Sortie_P.Holder_P.To_Holder
+               (Fabrique.Fabriquer_Sortie);
+      end;
 
       T.Chaine.Filtrer (Nom_Fichier, Extension);
 
@@ -97,7 +113,6 @@ package body Des_P.Chaine_P.Ravenscar_P.Test_P is
 
    ---------------------------------------------------------------------------
    procedure Test_Filtre_Decrypt (T : in out Test_Fixt_T) is
-      Const_Decrypt : Faiseur_Decryptage_P.Constructeur_Decryptage_T;
       Const_56 : Faiseur_56_P.Constructeur_Clef_T;
       Const_48 : Faiseur_48_P.Constructeur_Clef_T;
    begin
@@ -109,9 +124,35 @@ package body Des_P.Chaine_P.Ravenscar_P.Test_P is
          Lecteur_64_IO.Close (Fichier);
          pragma Unreferenced (Fichier);
       end;
-      Const_Decrypt.Initialiser (Const_56, Const_48);
-      Const_Decrypt.Construire (T.Clef);
-      T.Chaine := Chaine_T (Const_Decrypt.Recuperer_Chaine);
+      declare
+         Clef_56 : Des_P.Clef_P.Clef_56_I_P.Clef_Interface_T'Class :=
+            Des_P.Faiseur_P.Faire_Clef (Const_56, T.Clef);
+         Fabrique : Des_P.Filtre_P.Fabrique_P.Decryptage_P.Fabrique_T;
+         J : Numero_Filtre_T := Numero_Filtre_T'First;
+         use type Des_P.Clef_P.Clef_56_I_P.Decalage_T;
+      begin
+         T.Chaine.Filtre_Entree :=
+            Des_P.Filtre_P.Entree_P.Holder_P.To_Holder
+               (Fabrique.Fabriquer_Entree);
+
+         for I in reverse Numero_Filtre_T'Range loop
+            T.Chaine.Filtres_Corps (J) :=
+               Des_P.Filtre_P.Corps_P.Holder_P.To_Holder
+               (
+                  Fabrique.Fabriquer_Corps
+                  (Des_P.Faiseur_P.Faire_Clef (Const_48, Clef_56))
+               );
+            Clef_56.Decaler_Bits_A_Gauche (-1 * Table_Decalage (I));
+            if J /= Numero_Filtre_T'Last then
+               J := Numero_Filtre_T'Succ (J);
+            end if;
+         end loop;
+
+         --  Ajoute le filtre de sortie.
+         T.Chaine.Filtre_Sortie :=
+            Des_P.Filtre_P.Sortie_P.Holder_P.To_Holder
+               (Fabrique.Fabriquer_Sortie);
+      end;
 
       T.Chaine.Filtrer (Nom_Fichier, Extension);
 
@@ -140,7 +181,6 @@ package body Des_P.Chaine_P.Ravenscar_P.Test_P is
    procedure Test_Comparaison_Sequentiel_Crypt (T : in out Test_Fixt_T) is
       package Faiseur_Cryptage_Seq_P renames
          Des_P.Chaine_P.Sequentiel_P.Constructeur_Cryptage_P;
-      Const_Crypt : Faiseur_Cryptage_P.Constructeur_Cryptage_T;
       Const_Crypt_S : Faiseur_Cryptage_Seq_P.Constructeur_Cryptage_T;
       Const_56 : Faiseur_56_P.Constructeur_Clef_T;
       Const_48 : Faiseur_48_P.Constructeur_Clef_T;
@@ -156,9 +196,31 @@ package body Des_P.Chaine_P.Ravenscar_P.Test_P is
          Lecteur_64_IO.Close (Fichier);
          pragma Unreferenced (Fichier);
       end;
-      Const_Crypt.Initialiser (Const_56, Const_48);
-      Const_Crypt.Construire (T.Clef);
-      T.Chaine := Chaine_T (Const_Crypt.Recuperer_Chaine);
+      declare
+         Clef_56 : Des_P.Clef_P.Clef_56_I_P.Clef_Interface_T'Class :=
+            Des_P.Faiseur_P.Faire_Clef (Const_56, T.Clef);
+         Fabrique : Des_P.Filtre_P.Fabrique_P.Cryptage_P.Fabrique_T;
+      begin
+         T.Chaine.Filtre_Entree := Des_P.Filtre_P.Entree_P.Holder_P.To_Holder
+               (Fabrique.Fabriquer_Entree);
+
+         --  Ajoute le filtre de corps à la chaine.
+         for I in Numero_Filtre_T'Range loop
+            --  Décalage à gauche pour le cryptage.
+            Clef_56.Decaler_Bits_A_Gauche (Table_Decalage (I));
+            --  Initialise le filtre avec la clef de 48.
+            T.Chaine.Filtres_Corps (I) :=
+               Des_P.Filtre_P.Corps_P.Holder_P.To_Holder
+               (
+                  Fabrique.Fabriquer_Corps
+                  (Des_P.Faiseur_P.Faire_Clef (Const_48, Clef_56))
+               );
+         end loop;
+
+         --  Ajoute le filtre de sortie.
+         T.Chaine.Filtre_Sortie := Des_P.Filtre_P.Sortie_P.Holder_P.To_Holder
+               (Fabrique.Fabriquer_Sortie);
+      end;
 
       T.Chaine.Filtrer (Nom_Fichier, Extension);
 
@@ -222,7 +284,6 @@ package body Des_P.Chaine_P.Ravenscar_P.Test_P is
    procedure Test_Comparaison_Sequentiel_Decrypt (T : in out Test_Fixt_T) is
       package Faiseur_Decryptage_Seq_P renames
          Des_P.Chaine_P.Sequentiel_P.Constructeur_Decryptage_P;
-      Const_Decrypt : Faiseur_Decryptage_P.Constructeur_Decryptage_T;
       Const_Decrypt_S : Faiseur_Decryptage_Seq_P.Constructeur_Decryptage_T;
       Const_56 : Faiseur_56_P.Constructeur_Clef_T;
       Const_48 : Faiseur_48_P.Constructeur_Clef_T;
@@ -238,9 +299,35 @@ package body Des_P.Chaine_P.Ravenscar_P.Test_P is
          Lecteur_64_IO.Close (Fichier);
          pragma Unreferenced (Fichier);
       end;
-      Const_Decrypt.Initialiser (Const_56, Const_48);
-      Const_Decrypt.Construire (T.Clef);
-      T.Chaine := Chaine_T (Const_Decrypt.Recuperer_Chaine);
+      declare
+         Clef_56 : Des_P.Clef_P.Clef_56_I_P.Clef_Interface_T'Class :=
+            Des_P.Faiseur_P.Faire_Clef (Const_56, T.Clef);
+         Fabrique : Des_P.Filtre_P.Fabrique_P.Decryptage_P.Fabrique_T;
+         J : Numero_Filtre_T := Numero_Filtre_T'First;
+         use type Des_P.Clef_P.Clef_56_I_P.Decalage_T;
+      begin
+         T.Chaine.Filtre_Entree :=
+            Des_P.Filtre_P.Entree_P.Holder_P.To_Holder
+               (Fabrique.Fabriquer_Entree);
+
+         for I in reverse Numero_Filtre_T'Range loop
+            T.Chaine.Filtres_Corps (J) :=
+               Des_P.Filtre_P.Corps_P.Holder_P.To_Holder
+               (
+                  Fabrique.Fabriquer_Corps
+                  (Des_P.Faiseur_P.Faire_Clef (Const_48, Clef_56))
+               );
+            Clef_56.Decaler_Bits_A_Gauche (-1 * Table_Decalage (I));
+            if J /= Numero_Filtre_T'Last then
+               J := Numero_Filtre_T'Succ (J);
+            end if;
+         end loop;
+
+         --  Ajoute le filtre de sortie.
+         T.Chaine.Filtre_Sortie :=
+            Des_P.Filtre_P.Sortie_P.Holder_P.To_Holder
+               (Fabrique.Fabriquer_Sortie);
+      end;
 
       T.Chaine.Filtrer (Nom_Fichier, Extension);
 
