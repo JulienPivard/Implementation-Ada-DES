d8962ce0d1a2b51fe97d84f998986030eee6822a
Ythildir <pivardjulien@gmail.com>
2019-02-06 22:56:47 +0100

    Respect des conventions dans le main des tests unitaires

--- tests/main_test_harnais.adb
+++ tests/main_test_harnais.adb
@@ -15,43 +15,43 @@ with Des_P.Chaine_P.Ravenscar_P.Test_P;
 
 procedure Main_Test_Harnais is
 
-   procedure runner_suite_tests_bloc is new AUnit.Run.Test_Runner
+   procedure Runner_Suite_Tests_Bloc         is new AUnit.Run.Test_Runner
       (Suite_De_Tests_Bloc_P.Fonc_Suite);
-   procedure runner_suite_tests_clef is new AUnit.Run.Test_Runner
+   procedure Runner_Suite_Tests_Clef         is new AUnit.Run.Test_Runner
       (Suite_De_Tests_Clef_P.Fonc_Suite);
-   procedure runner_suite_tests_xor is new AUnit.Run.Test_Runner
+   procedure Runner_Suite_Tests_Xor          is new AUnit.Run.Test_Runner
       (Suite_De_Tests_Xor_P.Fonc_Suite);
-   procedure runner_suite_tests_etage is new AUnit.Run.Test_Runner
+   procedure Runner_Suite_Tests_Etage        is new AUnit.Run.Test_Runner
       (Suite_De_Tests_Etage_P.Fonc_Suite);
-   procedure runner_suite_tests_filtre is new AUnit.Run.Test_Runner
+   procedure Runner_Suite_Tests_Filtre       is new AUnit.Run.Test_Runner
       (Suite_De_Tests_Filtre_P.Fonc_Suite);
-   procedure runner_suite_tests_sequentiel is new AUnit.Run.Test_Runner
+   procedure Runner_Suite_Tests_Sequentiel   is new AUnit.Run.Test_Runner
       (Suite_De_Tests_Sequentiel_P.Fonc_Suite);
-   procedure runner_suite_tests_taches is new AUnit.Run.Test_Runner
+   procedure Runner_Suite_Tests_Taches       is new AUnit.Run.Test_Runner
       (Suite_De_Tests_Taches_P.Fonc_Suite);
-   procedure runner_suite_tests_ravenscar is new AUnit.Run.Test_Runner
+   procedure Runner_Suite_Tests_Ravenscar    is new AUnit.Run.Test_Runner
       (Suite_De_Tests_Ravenscar_P.Fonc_Suite);
 
-   options : constant AUnit.Options.AUnit_Options :=
+   Options : constant AUnit.Options.AUnit_Options :=
       (
          Global_Timer     => True,
          Test_Case_Timer  => True,
          Report_Successes => True,
          Filter           => null
       );
-   reporter : AUnit.Reporter.Text.Text_Reporter;
+   Reporter : AUnit.Reporter.Text.Text_Reporter;
 
 begin
-   AUnit.Reporter.Text.Set_Use_ANSI_Colors (reporter, True);
+   AUnit.Reporter.Text.Set_Use_ANSI_Colors (Reporter, True);
 
-   runner_suite_tests_bloc (reporter, options);
-   runner_suite_tests_clef (reporter, options);
-   runner_suite_tests_xor (reporter, options);
-   runner_suite_tests_etage (reporter, options);
-   runner_suite_tests_filtre (reporter, options);
-   runner_suite_tests_sequentiel (reporter, options);
-   runner_suite_tests_taches (reporter, options);
-   runner_suite_tests_ravenscar (reporter, options);
+   Runner_Suite_Tests_Bloc       (Reporter, Options);
+   Runner_Suite_Tests_Clef       (Reporter, Options);
+   Runner_Suite_Tests_Xor        (Reporter, Options);
+   Runner_Suite_Tests_Etage      (Reporter, Options);
+   Runner_Suite_Tests_Filtre     (Reporter, Options);
+   Runner_Suite_Tests_Sequentiel (Reporter, Options);
+   Runner_Suite_Tests_Taches     (Reporter, Options);
+   Runner_Suite_Tests_Ravenscar  (Reporter, Options);
 
    Des_P.Chaine_P.Ravenscar_P.Test_P.Test_Fin;
 
