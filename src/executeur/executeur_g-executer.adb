a7bf9a638084c2073c3b0bce9825987a965a4105
Ythildir <pivardjulien@gmail.com>
2024-05-26 17:26:59 +0200

    Déplacement code source tests unitaires

---
+++ src/executeur/executeur_g-executer.adb
@@ -0,0 +1,14 @@
+--  with GNAT.Source_Info;
+
+separate (Executeur_G)
+procedure Executer
+   --  (Arguments)
+is
+begin
+   --  Ada.Text_IO.Put      (Item => "Procédure : [");
+   --  Ada.Text_IO.Put      (Item => GNAT.Source_Info.Enclosing_Entity);
+   --  Ada.Text_IO.Put      (Item => "], une instance de : ");
+   --  Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Source_Location);
+   --  Mettez votre code ici
+   null;
+end Executer;
